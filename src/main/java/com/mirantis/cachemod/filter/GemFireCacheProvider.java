3059fc0c6ee17b3d54f484b2849e7ee8d4b4fca3
aschwid <aschwid@mirantis.com>
2011-08-22 17:57:52 +0400

    try catch

--- src/main/java/com/mirantis/cachemod/filter/GemFireCacheProvider.java
+++ src/main/java/com/mirantis/cachemod/filter/GemFireCacheProvider.java
@@ -16,18 +16,26 @@
 
 package com.mirantis.cachemod.filter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.gemstone.gemfire.cache.Cache;
 import com.gemstone.gemfire.cache.CacheFactory;
 import com.gemstone.gemfire.cache.Region;
 
 public class GemFireCacheProvider implements CacheProvider {
 
+  private static final Log log = LogFactory.getLog(GemFireCacheProvider.class);
+  
   private Cache cache;
   private Region<String, GemFireCacheEntry> region;
   
   @Override
   public void init(String cacheName) {
     cache = CacheFactory.getAnyInstance();
+    if (cache == null) {
+      throw new IllegalStateException("no cache in JVM");
+    }
     region = cache.getRegion(cacheName);   
   }
 
@@ -38,12 +46,23 @@ public class GemFireCacheProvider implements CacheProvider {
 
   @Override
   public CacheEntry getEntry(String key) {
-    return region.get(key);
+    try {
+      return region.get(key);
+    }
+    catch(Exception e) {
+      log.error("fail to get page from cache " + key, e);
+    }
+    return null;
   }
 
   @Override
   public void putEntry(String key, CacheEntry cacheEntry) {
-    region.put(key, (GemFireCacheEntry) cacheEntry);
+    try {
+      region.put(key, (GemFireCacheEntry) cacheEntry);
+    }
+    catch(Exception e) {
+      log.error("fail to put page to cache " + key, e);
+    }
   }
 
   @Override
