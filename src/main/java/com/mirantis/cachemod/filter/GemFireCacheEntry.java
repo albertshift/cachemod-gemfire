8ff1ac08f52227e1b23c5639b8e9e1e2ccb1fb5a
aschwid <aschwid@mirantis.com>
2011-08-22 17:45:03 +0400

    added key serialization

--- src/main/java/com/mirantis/cachemod/filter/GemFireCacheEntry.java
+++ src/main/java/com/mirantis/cachemod/filter/GemFireCacheEntry.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Mirantis Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.mirantis.cachemod.filter;
 
 import java.io.DataInput;
@@ -14,6 +30,7 @@ public class GemFireCacheEntry extends CacheEntry implements DataSerializable {
   
   @Override
   public void fromData(DataInput in) throws IOException, ClassNotFoundException {
+    key = in.readUTF();
     if (in.readBoolean()) {
       byte[] blob = new byte[in.readInt()];
       in.readFully(blob);
@@ -44,6 +61,7 @@ public class GemFireCacheEntry extends CacheEntry implements DataSerializable {
 
   @Override
   public void toData(DataOutput out) throws IOException {
+    out.writeUTF(key);
     out.writeBoolean(userData != null);
     if (userData != null) {
       byte[] userDataBlob = BlobHelper.serializeToBlob(userData);
